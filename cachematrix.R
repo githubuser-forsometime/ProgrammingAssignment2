66bca4dda5f8dfeea739f2b305abcf606ad615c1
githubuser-forsometime <33353592+githubuser-forsometime@users.noreply.github.com>
2018-01-13 03:19:54 +0530

    Add files via upload
    done as was required

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## creates a 'special' Matrix, ie cachematrix where it can have its own inverse associated with it.
+## if the inverse is asked of it, it searches for the inverse in the cache first thing; if found
+## returns it, else computes it, stores it in the cache and returns it.
 
-## Write a short comment describing this function
+## Creates a special matrix with 4 functions named get, set, getInvMat, setInvMat associated with it.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(Mat = matrix())    {
+    InvMat <<- NULL
 
+    setMat <- function(M)   {
+        Mat <<- M
+        InvMat <<- NULL
+    }
+    getMat <- function()    {
+        Mat
+    }
+
+    setInvMat <- function(InvMatrix)    {
+        InvMat <<- InvMatrix
+    }
+    getInvMat <- function() {
+        InvMat
+    }
+
+    list(set = setMat, get = getMat,
+         setInvMat = setInvMat, getInvMat = getInvMat )
 }
 
 
-## Write a short comment describing this function
+## if the inverse is already in the cache, the function retreives it and returns
+## otherwise computes the function computes the inverse, pushes to cache and returns it
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(M, ...)  {
+    ## Return a matrix that is the inverse of 'x'
+    Mat <- M$get()
+    if(is.matrix(M$getInvMat())) {
+        invMat <- M$getInvMat()
+    }
+    else    {
+        invMat <- solve(Mat)
+        M$setInvMat(invMat)
+    }
+
+    invMat
 }
